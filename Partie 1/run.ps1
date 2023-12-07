68ab0bf893dc5f6a0c083403b93fa628985b1a97
Mattys Gervais <mattysgervais@gmail.com>
2023-12-07 10:27:57 -0500

    output folder

--- Partie 1/run.ps1
+++ Partie 1/run.ps1
@@ -11,14 +11,14 @@ echo ''
 echo ''
 echo "########## Partie 1 - Question 1 ##########"
 echo ''
-python ./py/TypeTitre.py ./tsv/data.tsv > outputQ1.txt
+python ./py/TypeTitre.py ./tsv/data.tsv > output/Q1.txt
 
 echo ''
 echo "########## Partie 1 - Question 2 ##########"
 echo ''
-python ./py/NbFilmAnnees.py ./tsv/data.tsv > outputQ2.txt
+python ./py/NbFilmAnnees.py ./tsv/data.tsv > output/Q2.txt
 
 echo ''
 echo "########## Partie 1 - Question 3 ##########"
 echo ''
-python ./py/Interval.py ./tsv/ratings.tsv > outputQ3.txt
+python ./py/Interval.py ./tsv/ratings.tsv > output/Q3.txt
